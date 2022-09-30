c596e2c59767287f25630c7360cb21f1647a4e15
Esteban Acosta <estebanacosta19@gmail.com>
2022-09-30 00:05:06 -0400

    Added some more code for organization of items on page

--- 2048Clone/2048Clone.js
+++ 2048Clone/2048Clone.js
@@ -1,4 +1,17 @@
 function createRandomizedGrid()
 {
+    let buttons = [];
+
+    for(let row = 1; row < 5; row++)
+    {
+        let buttonRow = document.getElementById("row"+ String(row));
+
+        buttons.push(buttonRow.children);
+    }
+
+    // console.table(buttons);
+
 
 }
+
+createRandomizedGrid();

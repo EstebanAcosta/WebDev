2f9bb328c1fde0281a108eb5447802c9e9b0889d
Esteban Acosta <estebanacosta19@gmail.com>
2022-10-08 00:28:52 -0400

    Added code that would detected key presses

--- 2048Clone/2048Clone.js
+++ 2048Clone/2048Clone.js
@@ -1,118 +1,107 @@
-
 run();
 
-function run()
-{
+let score = 0;
+let bestScore = 0;
+
+function run() {
   createRandomizedGrid();
 
-  addEventListnerToNewGameButton();
-
-  addEventListenersToButtonsOnGrid();
-
+  addEventListenerToNewGameButton();
 }
 
 function createRandomizedGrid() {
-
-  let allButtons = [];
-
   // # of rows in the 2048 grid
   const numRow = 4;
 
   // # of columns in the 2048 grid
   const numCol = 4;
 
+  //create an array of buttons
+  let allButtons = [];
+
   //go through each row of the table
   for (let row = 0; row < numRow; row++) {
-    //grab the row
-    let thisRow = document.getElementById("row" + String(row + 1));
+    //grab the row of buttons
+    let thisRowOfButtons = document.getElementById("row" + String(row + 1));
 
-    //create an array of buttons
     //and push them into the 2D matrix
-    allButtons.push(thisRow.children);
-  }  
+    allButtons.push(thisRowOfButtons.children);
+  }
 
-  let randomRow, randomCol, randomNum, startNum;
+  let randomRow = 0;
+  let randomCol = 0;
+  let startNum = 0;
 
-  //loop two times to create two random numbers 
+  //loop two times to create two random numbers
   //and find a place for them on the grid
-  for(let i = 0 ; i < 2; i++)
-  {
+  for (let i = 0; i < 2; i++) {
     //get a random row (that is within the grid)
     randomRow = Math.floor(Math.random() * 4);
 
     //get a random column (that is within the grid)
     randomCol = Math.floor(Math.random() * 4);
 
-    //get a random number from 0 - 9
-    randomNum = Math.floor(Math.random() * 10);
-
     //if the random number is even, set the starting number to 2
     //otherwise, set the starting number to 4
-    startNum = randomNum % 2 === 0 ? 2 : 4;
+    startNum = Math.floor(Math.random() * 10) % 2 === 0 ? 2 : 4;
 
     //if a number has been placed in that specific spot
     //of the grid in the last iteration
-    if(i >=1 && (allButtons[randomRow][randomCol].value === 2 || 
-      allButtons[randomRow][randomCol].value === 4 ))
-    {
+    if (
+      i >= 1 &&
+      (allButtons[randomRow][randomCol].value === 2 ||
+        allButtons[randomRow][randomCol].value === 4)
+    ) {
       //subtract one from i so we can retry finding a place for the second number
-      i-=1;
+      i -= 1;
     }
-    //if that spot is vacation
-    else
-    {
-    //set that spot to the random value
-    allButtons[randomRow][randomCol].value = startNum;
+    //if that spot is vacant
+    else {
+      //set that spot to the random value
+      allButtons[randomRow][randomCol].value = startNum;
     }
-
-  
   }
 
+  window.addEventListener("keydown", checkKey);
 
-  console.table(allButtons);
+  //console.table(allButtons);
 }
 
-function addEventListenersToButtonsOnGrid()
-{
- // # of rows in the 2048 grid
- const numRow = 4;
-
- // # of columns in the 2048 grid
- const numCol = 4;
-
- //go through each row of the table
- for (let row = 0; row < numRow; row++) {
-
- }
+function checkKey(e) {
+  switch (e.keyCode) {
+    case 38:
+      //user pressed the up arrow
+      moveUp();
+      break;
+    case 40:
+      //user pressed the down arrow
+      moveDown();
+      break;
+    case 37:
+      //user pressed the left arrow
+      moveLeft();
+      break;
+    case 39:
+      //user pressed the right arrow
+      moveRight();
+      break;
+  }
 }
 
-function addEventListnerToNewGameButton()
-{
+function addEventListenerToNewGameButton() {}
 
+function moveUp() {
+  console.log("Pressed up");
 }
 
-
-function moveUp(e)
-{
-
+function moveDown() {
+  console.log("Pressed Down");
 }
 
-function moveDown(e)
-{
-  
+function moveRight() {
+  console.log("Pressed Right");
 }
 
-function moveRight(e)
-{
-  
+function moveLeft() {
+  console.log("Pressed Left");
 }
-
-function moveLeft(e)
-{
-  
-}
-
-
-
-
-
